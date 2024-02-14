b19fbd18ca91ea6e7f5f0da2ca3be7542403f8b8
brunogoniadis <brunogoniadislima@gmail.com>
2024-02-14 20:32:44 -0300

    fix modules

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,8 @@ import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-    return response.json({ message: "hello words!" });
+app.get('/', (request, response) => {
+  return response.json({ message: 'hello words!' });
 });
 
 app.listen(3333);
